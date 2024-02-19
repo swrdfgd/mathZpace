8466e94d9ef7c1568fa09945052720d5e51d8874
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2024-02-20 00:51:05 +0800

    Add files via upload

--- mathinfo.js
+++ mathinfo.js
@@ -1,3 +1,4 @@
 mathInfo = [
 "\\( \\sqrt{2} \\) is irrational.",
+"<b>Fundamental Theorem of Algebra:</b> Every polynomial equation over the field of complex numbers of degree higher than 1 has a complex solution.",
 ]
