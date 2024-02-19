601c8d73168658bb2399c7d531ca313855f983b2
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2024-02-20 00:54:19 +0800

    Add files via upload

--- mathinfo.js
+++ mathinfo.js
@@ -1,4 +1,6 @@
 mathInfo = [
 "\\( \\sqrt{2} \\) is irrational.",
 "<b>Fundamental Theorem of Algebra:</b> Every polynomial equation over the field of complex numbers of degree higher than 1 has a complex solution.",
+"<b>The Denumerability of the Rational Numbers:</b> The set \\(\\mathbb{Q} \\) of rational numbers is countably infinite.",
+
 ]
