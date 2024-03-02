db16cd2df5b39f270dd7bc5175a8a42d20d4f111
swrdfgd <32948834+swrdfgd@users.noreply.github.com>
2024-03-02 13:12:27 +0800

    Add files via upload

--- mathinfo.js
+++ mathinfo.js
@@ -2,5 +2,12 @@ mathInfo = [
 "\\( \\sqrt{2} \\) is irrational.",
 "<b>Fundamental Theorem of Algebra:</b> Every polynomial equation over the field of complex numbers of degree higher than 1 has a complex solution.",
 "<b>The Denumerability of the Rational Numbers:</b> The set \\(\\mathbb{Q} \\) of rational numbers is countably infinite.",
+'<b>Pythagorean Theorem: </b>The sum of (the areas of) the two small squares equals (the area of) the big one.<br><img src="img1.png" /><br>In algebraic terms, \\( a^2 + b^2 = c^2 \\) where \\(c\\) is the hypotenuse while \\(a\\) and \\(b\\) are the legs of the triangle.',
+"<b>Prime Number Theorem:</b> The number of primes not exceeding \\(x\\) is <b>asymptotic</b> to \\(\\frac{x}{\\ln x}\\) i.e \\( \\pi \\left ( x \\right ) \\sim \\frac{x}{\\ln x} \\).",
+"<b>Gödel Incompleteness Theorem:</b> For any formal axiomatic system, <i>there is always a statement about natural numbers which is true, but which cannot be proven in the system. </i>",
+"<b>Quadratic Reciprocity Theorem:</b> if \\(p\\) and \\(q\\) are are distict odd primes, then quadratic reciprocity theorem states that the congruences<br>\\(x^2 \\equiv q \\left ( \\text{mod } p \\right ) \\)<br>\\(x^2 \\equiv p \\left ( \\text{mod } q \\right ) \\)<br>are both solvable or both unsolvable unless both \\(p\\) and \\(q\\) leave remainder \\(3\\) when divided by \\(4\\).",
+"<b>Trisecting the Angle</b> and <b>Doubling the Cube</b> are generally impossible only by using ruler and compass.",
+"Area of a circle = \\(\\pi \\) × (radius)<sup>2</sup>.",
+"The Euler\'s totient function \\(\\varphi \\) for integer \\(m\\) is defined as the number of non-negative integers less than and coprime to \\(m\\).<br><b>Euler Generalisation of Fermat's Little Theorem:</b> Let \\(a\\) and \\(m\\) be coprime. Then \\(a^{\\varphi \\left ( m \\right )} \\equiv 1 \\left ( \\text{mod } m \\right ) \\).",
 
 ]
